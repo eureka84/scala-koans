fc1cfcad2cf45d6ff372721f758130ad3c494af4
eureka84 <angelosciarra@ymail.com>
2018-10-06 21:40:26 +0200

    Meditated on Pattern Matching

--- src/test/scala/org/functionalkoans/forscala/AboutPatternMatching.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutPatternMatching.scala
@@ -2,8 +2,9 @@ package org.functionalkoans.forscala
 
 import support.KoanSuite
 
-class AboutPatternMatching extends KoanSuite {
+import scala.util.matching.Regex
 
+class AboutPatternMatching extends KoanSuite {
 
   koan("Pattern matching returns something") {
 
@@ -16,7 +17,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => println(stuff); 0 //case _ will trigger if all other cases fail.
     }
 
-    myStuff should be(__)
+    myStuff should be(2)
 
   }
 
@@ -30,12 +31,11 @@ class AboutPatternMatching extends KoanSuite {
       case _ => println(stuff); 0
     }
 
-    myStuff should be(__, __, __)
+    myStuff should be(0, 0, 255)
 
   }
 
   koan("Pattern matching can match complex expressions") {
-
 
     def goldilocks(expr: Any) = expr match {
       case ("porridge", "Papa") => "Papa eating porridge"
@@ -44,7 +44,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Mama")) should be(__)
+    goldilocks(("porridge", "Mama")) should be("Mama eating porridge")
 
   }
 
@@ -57,8 +57,8 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("eating")
+    goldilocks(("chair", "Mama")) should be("sitting")
 
   }
 
@@ -71,13 +71,12 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("Papa said someone's been eating my porridge")
+    goldilocks(("chair", "Mama")) should be("Mama said someone's been sitting in my chair")
   }
 
-
   koan("Pattern matching can done on regular expression groups") {
-    val EatingRegularExpression = """Eating Alert: bear=([^,]+),\s+source=(.+)""".r //.r turns a String to a regular expression
+    val EatingRegularExpression: Regex = """Eating Alert: bear=([^,]+),\s+source=(.+)""".r //.r turns a String to a regular expression
     val SittingRegularExpression = """Sitting Alert: bear=([^,]+),\s+source=(.+)""".r
     val SleepingRegularExpression = """Sleeping Alert: bear=([^,]+),\s+source=(.+)""".r
 
@@ -88,25 +87,25 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks("Eating Alert: bear=Papa, source=porridge") should be(__)
-    goldilocks("Sitting Alert: bear=Mama, source=chair") should be(__)
+    goldilocks("Eating Alert: bear=Papa, source=porridge") should be("Papa said someone's been eating my porridge")
+    goldilocks("Sitting Alert: bear=Mama, source=chair") should be("Mama said someone's been sitting on my chair")
   }
 
   koan( """A backquote can be used to refer to a stable variable in scope to create a case statement.
           | This prevents what is called \'Variable Shadowing\'""") {
     val foodItem = "porridge"
 
-    def goldilocks(expr: Any) = expr match {
+    def goldilocks(expr: (String, String)) = expr match {
       case (`foodItem`, _) => "eating"
       case ("chair", "Mama") => "sitting"
       case ("bed", "Baby") => "sleeping"
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
-    goldilocks(("porridge", "Cousin")) should be(__)
-    goldilocks(("beer", "Cousin")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("eating")
+    goldilocks(("chair", "Mama")) should be("sitting")
+    goldilocks(("porridge", "Cousin")) should be("eating")
+    goldilocks(("beer", "Cousin")) should be("what?")
   }
 
   koan("A backquote can be used to refer to a method parameter as a stable variable to create a case statement.") {
@@ -115,9 +114,9 @@ class AboutPatternMatching extends KoanSuite {
       case `i` => true
       case _ => false
     }
-    patternEquals(3, 3) should be(__)
-    patternEquals(7, 9) should be(__)
-    patternEquals(9, 9) should be(__)
+    patternEquals(3, 3) should be(true)
+    patternEquals(7, 9) should be(false)
+    patternEquals(9, 9) should be(true)
   }
 
   koan(
@@ -129,18 +128,18 @@ class AboutPatternMatching extends KoanSuite {
       case _ => 0
     }
 
-    secondElement should be(__)
+    secondElement should be(2)
   }
 
   koan(
     """To obtain the second you can expand on the pattern. Where x is the first element, y
       | is the second element, and xs is the rest. """.stripMargin) {
     val secondElement = List(1,2,3) match {
-      case x :: y :: xs => xs
+      case x :: y :: xs => y
       case _ => 0
     }
 
-    secondElement should be(__)
+    secondElement should be(2)
   }
 
   koan(
@@ -150,7 +149,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => 0
     }
 
-    secondElement should be(__)
+    secondElement should be(0)
   }
 
   koan(
@@ -162,21 +161,6 @@ class AboutPatternMatching extends KoanSuite {
       case _ => 0
     }
 
-    r should be(__)
+    r should be(0)
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
