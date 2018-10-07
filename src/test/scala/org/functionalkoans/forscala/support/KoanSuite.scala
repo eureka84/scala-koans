a43f763fdb04b6b84587966bca4653f1ab439173
asciarra <angelo.sciarra@lastminute.com>
2018-10-07 20:30:55 +0200

    fixed some pending issues

--- src/test/scala/org/functionalkoans/forscala/support/KoanSuite.scala
+++ src/test/scala/org/functionalkoans/forscala/support/KoanSuite.scala
@@ -1,9 +1,19 @@
 package org.functionalkoans.forscala.support
 
-import org.scalatest._
+import org.scalatest.exceptions.TestPendingException
+import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.matchers.Matcher
 
 trait KoanSuite extends FunSuite with Matchers {
 
   def koan(name : String)(fun: => Unit) { test(name.stripMargin('|'))(fun) }
+
+  def  __ : Matcher[Any] = {
+    throw new TestPendingException
+  }
+
+  protected class ___ extends Exception {
+    override def toString = "___"
+  }
 
 }
